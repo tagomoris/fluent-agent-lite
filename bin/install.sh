848985c57ff35add01163fdfa42cb975e4432df2
TAGOMORI Satoshi <tagomoris@gmail.com>
2012-03-12 15:59:33 +0900

    fix if syntax

--- bin/install.sh
+++ bin/install.sh
@@ -27,6 +27,6 @@ cd $SOURCEDIR
 cp package/fluent-agent-lite.init /etc/init.d/fluent-agent-lite
 chmod +x /etc/init.d/fluent-agent-lite
 
-if [ ! -f /etc/fluent-agent-lite.conf -o "x"$FORCE = x"y" ]; 
+if [ ! -f /etc/fluent-agent-lite.conf -o "x"$FORCE = x"y" ]; then
     cp package/fluent-agent-lite.conf /etc/fluent-agent-lite.conf
 fi
