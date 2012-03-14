f4eb737d1f39a328c1d2ea257aa62d9a847dc48d
TAGOMORI Satoshi <tagomoris@gmail.com>
2012-03-14 16:01:10 +0900

    more fix to correct rpmbuld

--- bin/install.sh
+++ bin/install.sh
@@ -25,11 +25,13 @@ cp -rp bin lib Makefile.PL $INSTALLDIR
 
 cd $INSTALLDIR
 
+export PERL_CPANM_OPT="--local-lib=~/perl5"
 perl $SOURCEDIR/bin/cpanm -n inc::Module::Install
 perl $SOURCEDIR/bin/cpanm -lextlib -n --installdeps .
 
 cd $SOURCEDIR
 
+mkdir -f -p $PREFIX/etc/init.d
 cp package/fluent-agent-lite.init $PREFIX/etc/init.d/fluent-agent-lite
 chmod +x $PREFIX/etc/init.d/fluent-agent-lite
 
